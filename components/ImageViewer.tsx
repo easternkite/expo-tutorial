1addb9b8244be894ca82a39026f84fab5f5ddfdb
easternkite <easternkite7@gmail.com>
2025-04-10 21:56:19 +0900

    commit chapter4

---
+++ components/ImageViewer.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import { Image, type ImageSource } from "expo-image";
+
+type Props = {
+    imageSource: ImageSource;
+    selectedImage?: string;
+};
+
+export default function ImageViewer({ imageSource, selectedImage }: Props) {
+    const imgSource = selectedImage ? { uri: selectedImage } : imageSource;
+    return <Image source = {imgSource} style = {styles.image} />
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18
+    }
+});
